1fc871522794e52674b87d2b15418244e67b9d20
schausse-hash <schausse@gmail.com>
2026-03-11 22:00:23 -0400

    Delete components/BlogTracker.jsx

--- components/BlogTracker.jsx
+++
@@ -1,9 +0,0 @@
-'use client'
-import { useEffect } from 'react'
-import { useAnalytics } from '@/hooks/useAnalytics'
-
-export default function BlogTracker() {
-  const { track } = useAnalytics()
-  useEffect(() => { track('page_view') }, [track])
-  return null
-}
